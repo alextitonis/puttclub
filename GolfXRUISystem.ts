30949e99320a66967114582b2c37c21877b6b210
Josh Field <joshfield999@gmail.com>
2021-10-01 09:38:27 +1000

    fix ui system running on node

--- GolfXRUISystem.ts
+++ GolfXRUISystem.ts
@@ -1,10 +1,17 @@
+import { isClient } from '@xrengine/engine/src/common/functions/isClient'
 import { World } from '@xrengine/engine/src/ecs/classes/World'
-import { GolfPlayerUISystem } from './GolfPlayerUISystem'
-import { GolfScorecardUISystem } from './GolfScorecardUISystem'
 
 export default async function GolfXRUISystem(world: World) {
+
+  // TODO: this is temp, until reality packs have browser & node options
+  if(!isClient) return () => {}
+
+  const { GolfPlayerUISystem } = await import('./GolfPlayerUISystem')
+  const { GolfScorecardUISystem } = await import('./GolfScorecardUISystem')
+
   const playerUISystem = await GolfPlayerUISystem(world)
   const scoreboardUISystem = await GolfScorecardUISystem(world)
+
   return () => {
     playerUISystem()
     scoreboardUISystem()
